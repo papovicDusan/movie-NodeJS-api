58aa8171e221849589901f6b3ab70353955a3365
dusanPapovic <dusan.papovic@vivifyideas.com>
2022-11-25 13:33:22 +0100

    fix: delete user watchlist function

--- controllers/watchlist.js
+++ controllers/watchlist.js
@@ -50,42 +50,12 @@ exports.deleteWatchlist = async (req, res, next) => {
     await user.save();
 
     const movie = await Movie.findById(watchlist.movie);
-
     movie.watchlists = movie.watchlists.filter(
       (watchlistItem) => watchlistItem.toString() != watchlist._id.toString()
     );
     await movie.save();
 
     await watchlist.remove();
-
-    res.status(200).json({ message: "Deleted watchlist." });
-
-    // const movieId = req.params.movieId;
-
-    // const watchlist = await Watchlist.findOne({
-    //   user: req.userId,
-    //   movie: movieId,
-    // });
-    // if (!watchlist) {
-    //   const error = new Error("Could not find watchlist.");
-    //   error.statusCode = 404;
-    //   throw error;
-    // }
-
-    // await Watchlist.deleteOne({ _id: watchlist._id });
-
-    // const user = await User.findById(req.userId);
-    // user.watchlists = user.watchlists.filter(
-    //   (watchlistItem) => watchlistItem.toString() != watchlist._id.toString()
-    // );
-    // await user.save();
-
-    // const movie = await Movie.findById(movieId);
-
-    // movie.watchlists = movie.watchlists.filter(
-    //   (watchlistItem) => watchlistItem.toString() != watchlist._id.toString()
-    // );
-    // await movie.save();
 
     res.status(200).json({ message: "Deleted watchlist." });
   } catch (err) {
@@ -104,54 +74,7 @@ exports.setWatchlistWatched = async (req, res, next) => {
     watchlist.is_watched = isWatched;
     await watchlist.save();
 
-    // const movieId = req.params.movieId;
-    // const isWatched = req.body.is_watched;
-
-    // const watchlist = await Watchlist.findOne({
-    //   user: req.userId,
-    //   movie: movieId,
-    // });
-    // if (!watchlist) {
-    //   const error = new Error("Could not find watchlist.");
-    //   error.statusCode = 404;
-    //   throw error;
-    // }
-
-    // watchlist.is_watched = isWatched;
-    // await watchlist.save();
-
     res.status(200).json(watchlist);
-  } catch (err) {
-    if (!err.statusCode) {
-      err.statusCode = 500;
-    }
-    next(err);
-  }
-};
-
-exports.getUserWithWatchlists = async (req, res, next) => {
-  try {
-    const userId = req.params.userId;
-
-    console.log("req.params.userId", req.params.userId);
-
-    const userWatchlist = await Watchlist.aggregate([
-      {
-        $lookup: {
-          from: "movies",
-          localField: "movie",
-          foreignField: "_id",
-          as: "movieObject",
-        },
-      },
-      {
-        $match: {
-          user: mongoose.Types.ObjectId(userId),
-        },
-      },
-    ]);
-
-    res.status(200).json(userWatchlist);
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
--- routes/watchlist.js
+++ routes/watchlist.js
@@ -7,8 +7,6 @@ const router = express.Router();
 
 router.post("/", isAuth, watchlistController.createWatchlist);
 
-router.get("/users/:userId", isAuth, watchlistController.getUserWithWatchlists);
-
 router.delete("/:watchlistId", isAuth, watchlistController.deleteWatchlist);
 
 router.put("/:watchlistId", isAuth, watchlistController.setWatchlistWatched);
